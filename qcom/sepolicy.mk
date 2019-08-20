efd1dedf155c0bb1beadd10a1ccf2afd2aac21c3
dianlujitao <dianlujitao@lineageos.org>
2019-08-20 13:02:57 +0200

    sepolicy: Move livedisplay FOD and camera_motor policy to dynamic
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,17 @@
 # that inherit from Beast
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/beast/sepolicy/qcom/private
 
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/beast/sepolicy/qcom/common \
     device/beast/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
